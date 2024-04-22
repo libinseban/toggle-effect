436c2b35419507ae63c6bbe4baa6414c77ac4ba6
Libin Seban <libinseban97@gmail.com>
2024-04-22 23:05:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BulbContainer, Button, Bulb, BulbBottom, BulbStyle } from "./Components/StyledBulb";
 
-function App() {
+const App = () => {
+  const [isOn1, setIsOn1] = useState(false);
+  const [isOn2, setIsOn2] = useState(false);
+
+  const toggleBulb1 = () => {
+    setIsOn1(!isOn1);
+  };
+
+  const toggleBulb2 = () => {
+    setIsOn2(!isOn2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BulbContainer>
+
+      <BulbStyle>
+        <div>
+          <Bulb isOn={isOn1} />
+        </div>
+        <BulbBottom>
+        </BulbBottom>
+        <Button onClick={toggleBulb1}>{isOn1 ? 'OFF' : 'ON'}</Button>
+      </BulbStyle>
+      <BulbStyle>
+        <div>
+          <Bulb isOn={isOn2} />
+        </div>
+        <BulbBottom>
+        </BulbBottom>
+        <Button onClick={toggleBulb2}>{isOn2 ? 'OFF' : 'ON'}</Button>
+      </BulbStyle>
+    </BulbContainer>
   );
-}
+};
 
 export default App;
